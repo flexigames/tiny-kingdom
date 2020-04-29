12c65b2ca27be4256ff91e5c41bcf6ac6718cd18
Finn Pauls <mbp@pauls.dev>
2020-04-29 19:37:29 +0200

    Add building spots with towers

--- entities/Spot.js
+++ entities/Spot.js
@@ -1,8 +1,22 @@
-import Entity from "./Entity";
+import Entity from "./Entity"
+import * as PIXI from 'pixi.js'
 
 export default class Spot extends Entity {
   constructor(x, y, opts = {}) {
-    super(x, y, { sprite: 'selection-circle', ...opts })
+    super(x, y, { sprite: 'selection-circle', spriteAnchor: [0, 0], ...opts })
+
+
+    this.sprite.hitArea = new PIXI.Rectangle(-3, 0, 4, 4)
+
+    this.sprite.interactive = true
+    this.sprite.buttonMode = true
+
+    this.sprite.on('mouseup', () => {
+      this.changeTexture('tower1')
+      this.sprite.interactive = false
+      this.sprite.buttonMode = false
+      this.setPosition(this.pos.x, this.pos.y - 5)
+    })
   }
 
 }
