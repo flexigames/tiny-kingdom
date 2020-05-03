8f22d92ddaf23f8b075013eba9675e77b2ecb6d6
Finn Pauls <mbp@pauls.dev>
2020-05-03 20:45:57 +0200

    show field area of mill

--- entities/Building.js
+++ entities/Building.js
@@ -1,22 +1,50 @@
 import * as PIXI from 'pixi.js'
 import Entity from './Entity'
 import V from '../lib/vec2'
+import Grid from '../lib/Grid'
 
 export default class Building extends Entity {
   constructor(x, y, opts = {}) {
     super(x, y, opts)
-    const { offset = V(0, 0) } = opts
+    const { offset = V(0, 0), grid } = opts
+
+    this.grid = grid
 
     this.sprite.alpha = 0.5
 
     this.offsetX = offset.x
     this.offsetY = offset.y
 
+    this.gridPos = V(0, 0)
+
     this.sprite.anchor.set(
       0.5,
       1 - this.offsetY / (this.sprite.height / Entity.SCALE_FACTOR)
     )
   }
+
+  setGridPosition(x, y) {
+    this.gridPos = V(x, y)
+    this.setPosition((x + 0.5) * Grid.TILE_SIZE + Grid.OFFSET.x, (y + 1) * Grid.TILE_SIZE + Grid.OFFSET.y)
+  }
+
+  getNeighborGridPositions() {
+    return [
+      V(-1, -1),
+      V(-1, 0),
+      V(-1, 1),
+      V(0, -1),
+      V(0, 1),
+      V(1, -1),
+      V(1, 0),
+      V(1, 1)
+    ].map(pos => pos.add(this.gridPos))
+  }
+
+  findNeighborTiles() {
+    return this.getNeighborGridPositions.map(position => this.grid.get(position)).fiter(tile => tile)
+  }
+
   place() {
     if (Entity.level.money >= this.price) {
       Entity.level.money -= this.price
--- entities/Windmill.js
+++ entities/Windmill.js
@@ -1,6 +1,7 @@
 import Building from './Building'
 import Entity from './Entity'
 import V from '../lib/vec2'
+import Grid from '../lib/Grid'
 
 export default class Windmill extends Building {
   constructor(x, y, opts = {}) {
@@ -12,6 +13,39 @@ export default class Windmill extends Building {
     this.coinCounter = 0
 
     this.placed = false
+
+
+
+    // Grid.TILE_SIZE
+
+    // const rect = new PIXI.Graphics()
+    // rect.beginFill(0xfff6d3)
+    // rect.drawRect(posX, posY, this.tileSize, this.tileSize)
+    // rect.endFill()
+  }
+
+  setGridPosition(x, y) {
+    super.setGridPosition(x, y)
+
+    this.setNeighborMarkings()
+  }
+
+  place() {
+    super.place()
+    this.setNeighborMarkings(true)
+  }
+
+  setNeighborMarkings(remove = false) {
+    const neighborPositions = this.getNeighborGridPositions()
+    this.grid.iterate(({ x, y, field }) => {
+      const isNeighbor = neighborPositions.some(pos => pos.equals(V(x, y)))
+      if (isNeighbor && !remove) {
+        field.rectangle.alpha = 0.5
+        field.rectangle.tint = field.type ? 0xec6c70 : 0xfff6d3
+      } else {
+        field.rectangle.alpha = 0
+      }
+    })
   }
 
   update(dt) {
